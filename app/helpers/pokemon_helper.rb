103ace50768482740c660848b024266a55e2d9ab
Greg Shenefelt <60129025+greg0rys@users.noreply.github.com>
2026-04-21 13:47:32 -0700

    Refactor PokemonHelper and add new methods
    Refactor PokemonHelper methods for better organization and clarity. Added new methods for getting types, weaknesses, and evolution chains.

--- app/helpers/pokemon_helper.rb
+++ app/helpers/pokemon_helper.rb
@@ -1,43 +1,117 @@
+require 'httparty'
+require 'json'
 module PokemonHelper
-    $pokemon_endpoint = 'https://pokeapi.co/api/v2/pokemon/'
-    $item_endpoint = 'https://pokeapi.co/api/v2/item/'
+  $default_pokemon = HTTParty.get('https://pokeapi.co/api/v2/pokemon/jynx')
+  $default_pokemon_id = 124 # pokedex id for jynx
 
-    def get_all_pokemon
-        response = HTTParty.get($pokemon_endpoint)
-        return response.parsed_response['results']
+  def get_types
+    type_map = {}
+
+    res = HTTParty.get($type_endpoint)
+    return nil if res.blank?
+
+    res["results"].each do |type|
+      type_map[type["name"]] = type["url"]
     end
 
-    def get_pokemon_details(pokemon_name)
-        response = HTTParty.get($pokemon_endpoint << pokemon_name)
-        return nil if response.blank? || response.code != 200
+    type_map
+  end
 
-        response.parsed_response
+  def get_weaknesses(pokemon = $default_pokemon)
+  end
+
+  def get_all_pokemon
+    poke_map = {}
+    res = HTTParty.get($pokemon_all_endpoint)
+
+    res["results"].each do |p|
+      name = p["name"]
+      url = p["url"]
+      poke_map[p['name']] = p['url']
     end
 
-    def pokemon_image(pokemon_details)
-        pokemon_details['sprites']['front_default']
+    poke_map
+  end
+
+  # get a pokemon by name we will use tangela as the default if no param
+  def get_pokemon_by_name(name = "tangela")
+    response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/" << name.downcase)
+    pokemon = response.parsed_response #httparty alows us to get the parsed response as a ruby hash.
+    return nil if pokemon.blank?
+
+    pokemon.parsed_response
+
+  end
+
+  # by default get info for tangela note lets make these build pokemon objects for the application
+  def get_pokemon_by_poke_id(id = $default_pokemon_id)
+    response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{id}")
+    pokemon = response.parsed_response
+    return nil if pokemon.blank?
+
+    pokemon.parsed_response
+  end
+
+  def get_pokemon_by_type(type = 'fairy')
+    res = HTTParty.get("https://pokeapi.co/api/v2/type/#{type}")
+
+    return nil if res.blank?
+
+    res.parsed_response
+
+  end
+
+
+  # parser for search by name and id creates new pokemon obj
+  # for each array you go into like ability items etc you loop through and output the key value name so json returns abilities {[ability: name ]} so you parse as so
+  def parse_single_pokemon_results(pokemon = $default_pokemon)
+    abilities = pokemon['abilities']
+    puts "#{pokemon['name']} has a total of #{abilities.length}"
+    abilities.each do |a|
+      puts a['ability']['name']
     end
+  end
 
-    def get_pokemon_evolution_chain(pokemon_details)
-        species_response = HTTParty.get(pokemon_details['species']['url'])
-        return nil if species_response.blank? || species_response.code != 200
-        evolution_chain_url = species_response.parsed_response['evolution_chain']['url']
-        evolution_chain_response = HTTParty.get(evolution_chain_url)
-        return nil if evolution_chain_response.blank? || evolution_chain_response.code != 200
-        evolution_chain_response.parsed_response['chain']
-        
-
-    def get_all_items
-        response = HTTParty.get($item_endpoint)
-        return nil if response.blank?
-        response.parsed_response['results']
+  # get all known abilites for a pokemon 
+  def get_pokemon_abilities(pokemon = $default_pokemon)
+    pokemon['abilities'].each do |a|
+      puts a['ability']['name']
     end
+  end
 
-    def get_item_details(item_name)
-        response = HTTParty.get($item_endpoint << item_name)
-        return nil if response.blank?
-        response.parsed_response
+  def get_pokemon_artwork(pokemon = $default_pokemon)
+    puts pokemon['sprites']['other']['home']['front_default']
+  end
+
+  def get_held_items(pokemon = $default_pokemon)
+    pokemon['held_items'].each do |item|
+      puts "Held item name: #{item['item']['name']}"
+      puts "url: #{item['item']['url']}"
     end
+  end
 
+  def get_pokemon_evolution_chain(pokemon = $default_pokemon)
+    evolution_map = {}
+    evolution_chain_url = pokemon['species']['url']
+    res = HTTParty.get(evolution_chain_url)
+    return nil if res.blank?
+
+    evolution_chain = res['evolution_chain']['url']
+    res2 = HTTParty.get(evolution_chain)
+    return nil if res2.blank?
+
+    puts "Evolution chain for #{pokemon['name']}:"
+    res2['chain']['evolves_to'].each do |evolution|
+      puts evolution['species']['name']
+    end
+  end
 
 end
+
+
+=begin
+run all methods as test with the default pokemon (jynx)
+no param needed as default pokemon is passed in as def
+=end
+
+
