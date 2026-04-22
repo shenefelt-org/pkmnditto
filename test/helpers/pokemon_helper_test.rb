1e75da21a1a31fe77dac7077982f48afe0209178
Greg Shenefelt <greg@shenefelt.net>
2026-04-22 15:27:54 -0700

    l

--- test/helpers/pokemon_helper_test.rb
+++ test/helpers/pokemon_helper_test.rb
@@ -1,4 +1,10 @@
 require "test_helper"
 
 class PokemonHelperTest < ActionView::TestCase
+    test "should get types" do
+        types = get_types
+        assert_not_nil types
+        assert types.is_a?(Array)
+        assert types.any? { |t| t.keys.first == "fire" }
+    end
 end
