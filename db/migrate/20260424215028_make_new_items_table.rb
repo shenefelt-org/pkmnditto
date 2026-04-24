a1fa24694b8027614c155f2227e2648b5cfe0175
Greg Shenefelt <greg@shenefelt.net>
2026-04-24 14:51:54 -0700

    fil out migration

--- db/migrate/20260424215028_make_new_items_table.rb
+++ db/migrate/20260424215028_make_new_items_table.rb
@@ -1,4 +1,17 @@
 class MakeNewItemsTable < ActiveRecord::Migration[8.1]
   def change
+    # delete teh curr items table
+    drop_table :items, if_exists: true
+
+    create_table :items do |t|
+      t.string :name
+      t.string :url
+      t.string :sprite
+      t.text :generations # sereilze in model to keep array
+      t.string :flavor_text
+      t.string :short_effect
+
+      t.timestamps
+    end
   end
 end
