e1bab9a49908e718f043d4311d7cc702d90c5dc2
Greg Shenefelt <greg@shenefelt.net>
2026-04-24 08:30:58 -0700

    add copy method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,13 @@
 class Item < ApplicationRecord
   include ItemsHelper
 
-  def initialize(node: item_node = nil)
-    if !item_node.nil?
-      self.name = item_node['name']
-      self.url = item_node['url']
-      self.sprite = item_node['sprites']['default']
-      self.generations = get_game_versions(item: item_node)
-      self.short_effect = get_short_effect(item: item_node)
-    else
-      self.name = $def_item
-      self.url = "#{$item_endpoint}#{$def_item}"
-      self.sprite = nil
-      self.generations = nil
-      self.short_effect = nil
-    end
+  def copy(node: item_node = nil)
+    return nil if node.nil?
+    self.name = item_node['name']
+    self.url = item_node['url']
+    self.sprite = item_node['sprites']['default']
+    self.generations = get_game_versions(item: item_node)
+    self.short_effect = get_short_effect(item: item_node)
   end
+
 end
