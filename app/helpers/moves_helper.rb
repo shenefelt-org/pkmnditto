1f843dee9372077e66b0eb73298888bac08a2ce4
Greg Shenefelt <greg@shenefelt.net>
2026-04-24 23:29:11 -0700

    fix move endpoint typo

--- app/helpers/moves_helper.rb
+++ app/helpers/moves_helper.rb
@@ -1,5 +1,5 @@
 module MovesHelper
-  $move_endpoint = "https://pokeco.api/api/v2/move/"
+  $move_endpoint = "https://pokeapi.co/api/v2/move/"
   def build_items_map
     moves = HTTParty.get($move_endpoint)
     map = moves.map {|m| {name: m['name'], url: m['url']}}
