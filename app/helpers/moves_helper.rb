677a73472b7fde2bc7626d480de77739f768333d
Greg Shenefelt <greg@shenefelt.net>
2026-04-25 12:57:40 -0700

    httparty no longer allows .nil? ned to use .empty on responses.

--- app/helpers/moves_helper.rb
+++ app/helpers/moves_helper.rb
@@ -22,7 +22,7 @@ module MovesHelper
   def make_move_node(move_url: nil)
     return nil if move_url.nil?
     move_dat = get_move_by_url(url: move_url)
-    return nil if move_dat.nil?
+    return nil if move_dat.empty?
     short_effect = move_dat['effect_entries'].find { |entry| entry['language']['name'] == 'en' }
     return {
       name: move_dat['name'],
