692ddf0e4e95a36240c03fc8d5e9ed997f2c7258
Greg Shenefelt <greg@shenefelt.net>
2026-04-24 23:01:47 -0700

    had to change to type to move type since the word type is reserved

--- db/migrate/20260425055917_make_new_moves_table.rb
+++ db/migrate/20260425055917_make_new_moves_table.rb
@@ -1,4 +1,14 @@
 class MakeNewMovesTable < ActiveRecord::Migration[8.1]
   def change
+    drop_table :moves, if_exists: true
+    create_table :moves do |t|
+      t.string :name
+      t.string :url
+      t.string :move_type
+      t.integer :power
+      t.string :short_text
+      t.timestamps
+    end
+
   end
 end
