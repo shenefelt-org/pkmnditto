e213196b618118f459ee84023c3c40615d385612
Greg Shenefelt <greg@shenefelt.org>
2026-04-25 15:29:21 -0700

    db entry-damage-relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_25_211449) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_25_222418) do
+  create_table "damage_relations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "double_damage_from"
+    t.text "double_damage_to"
+    t.text "half_damage_from"
+    t.text "half_damage_to"
+    t.text "no_damage_from"
+    t.text "no_damage_to"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "flavor_text"
