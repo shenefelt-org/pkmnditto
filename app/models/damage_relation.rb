38bcbb9a94984be93db7a20bcad6dea67e9bb40b
Greg Shenefelt <greg@shenefelt.net>
2026-04-27 14:41:18 -0700

    rr

--- app/models/damage_relation.rb
+++ app/models/damage_relation.rb
@@ -1,8 +1,8 @@
 class DamageRelation < ApplicationRecord
-    serialize :half_damage_to, :string, array: true, default: [], coder: JSON
-    serialize :half_damage_from, :string, array: true, default: [], coder: JSON
-    serialize :double_damage_to, :string, array: true, default: [], coder: JSON
-    serialize :double_damage_from, :string, array: true, default: [], coder: JSON
-    serialize :no_damage_to, :string, array: true, default: [], coder: JSON
-    serialize :no_damage_from, :string, array: true, default: [], coder: JSON
+  serialize :half_damage_to, coder: JSON
+  serialize :half_damage_from, coder: JSON
+  serialize :double_damage_to, coder: JSON
+  serialize :double_damage_from, coder: JSON
+  serialize :no_damage_to, coder: JSON
+  serialize :no_damage_from, coder: JSON
 end
