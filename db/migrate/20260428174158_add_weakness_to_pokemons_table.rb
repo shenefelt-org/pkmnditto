77aa03fc5a383e5f86be9f83306d0c4e3160206d
Greg Shenefelt <greg@shenefelt.net>
2026-04-28 12:27:06 -0700

    f

--- db/migrate/20260428174158_add_weakness_to_pokemons_table.rb
+++ db/migrate/20260428174158_add_weakness_to_pokemons_table.rb
@@ -1,6 +1,6 @@
 class AddWeaknessToPokemonsTable < ActiveRecord::Migration[8.1]
   def change
-    add_reference :pokemons_tables, :type, null: false, foreign_key: true
-    add_reference :pokemons_tables, :pokemon, null: false, foreign_key: true
+    add_reference :pokemons_table, :type, null: false, foreign_key: true
+    add_reference :pokemons_table, :pokemon, null: false, foreign_key: true
   end
 end
