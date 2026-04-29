0b28fbb8d6325cca6abe76cbcc1e51c61bb4004a
Greg Shenefelt <greg@shenefelt.net>
2026-04-29 12:49:05 -0700

    tr

--- app/models/move.rb
+++ app/models/move.rb
@@ -2,7 +2,6 @@ class Move < ApplicationRecord
     belongs_to :type
     has_many :pokemon_moves
     has_many :learned_by, through: :pokemon_moves, source: :pokemon # define m:m relationship
-    has_many :weaknesses, through: :move_weaknesses, source: :type # define m:m relationship
     has_one :damage, through: :type, source: :damage_relation # define m:m relationship
 
 end
