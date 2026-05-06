f6cd5f011836949ea2132b8dfbd2c66838cb0e0e
Greg Shenefelt <greg@shenefelt.net>
2026-05-06 10:43:19 -0700

    nil params

--- script/build_app.rb
+++ script/build_app.rb
@@ -139,28 +139,7 @@ end
 
 begin_process
 
-def assign_learned_by
-  moves = Move.all
-  moves.each do |move|
-    move_datum = HTTParty.get(move.url)
-    next unless move_datum.success? && move_datum.parsed_response.is_a?(Hash)
 
-    details = move_datum.parsed_response
-    if details["learned_by_pokemon"]
-      details["learned_by_pokemon"].each do |ld|
-        pokemon = Pokemon.find_by(name: ld["name"])
-        next if pokemon.nil?
-
-        PokemonMove.create(
-          pokemon_id: pokemon.poke_id,
-          move_id: move.id
-        )
-      end
-    end
-  end
-
-  return true
-end
 
 table_data = [
   ["Pokemon", Pokemon.count, !Pokemon.count.zero? ? @pastel.green("SUCCESS") : @pastel.red("FAILED")],
--- script/move_test.rb
+++ script/move_test.rb
@@ -1,3 +1,27 @@
 include MovesHelper
 
-assign_learned_by()
+def assign_learned_by
+  moves = Move.all
+  moves.each do |move|
+    move_datum = HTTParty.get(move.url)
+    next unless move_datum.success? && move_datum.parsed_response.is_a?(Hash)
+
+    details = move_datum.parsed_response
+    if details["learned_by_pokemon"]
+      details["learned_by_pokemon"].each do |ld|
+        pokemon = Pokemon.find_by(name: ld["name"])
+        next if pokemon.nil?
+
+        PokemonMove.create(
+          pokemon_id: pokemon.poke_id,
+          move_id: move.id
+        )
+      end
+    end
+  end
+
+  return true
+end
+
+# this takes nil params 
+assign_learned_by
