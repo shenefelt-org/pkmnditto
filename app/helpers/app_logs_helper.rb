c16e01d9f1065b695f654e1d1db49fbf81bb9cda
Greg Shenefelt <60129025+greg0rys@users.noreply.github.com>
2026-05-08 09:08:51 -0700

    Update app_logs_helper.rb

--- app/helpers/app_logs_helper.rb
+++ app/helpers/app_logs_helper.rb
@@ -1,2 +1,24 @@
+unless defined?(Fixnum)
+  Fixnum = Integer
+end
+
+unless defined?(Bignum)
+  Bignum = Integer
+end
+
+require 'tty-table'
+
 module AppLogsHelper
+  def print_table
+    # Initialize the table with headers
+    table = TTY::Table.new(header: ['level', 'method', 'path', 'ip_address'])
+
+    # Add the data from your 'AppLog' model
+    AppLog.all.each do |row|
+      table << [row[:level], row[:method], row[:path], row[:ip_address]]
+    end
+
+    # Render the table
+    puts table.render(:ascii)
+  end
 end
